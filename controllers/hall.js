efbd269de40ef991cd68a6680c4a67bed05f7830
Vyshnavi1410 <vyshnavikattamuri14@gmail.com>
2022-11-16 14:21:29 -0600

    Delete view

--- controllers/hall.js
+++ controllers/hall.js
@@ -136,4 +136,17 @@ exports.hall_update_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+
+// Handle a delete one view with id from query
+exports.hall_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await hall.findById(req.query.id)
+    res.render('halldelete', { title: 'Hall Delete', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
    
--- routes/hall.js
+++ routes/hall.js
@@ -7,10 +7,13 @@ router.get('/', hall_controlers.hall_view_all_Page );
 
 router.get('/detail', hall_controlers.hall_view_one_Page); 
 
-/* GET hall costume page */
+/* GET hall hall page */
 router.get('/create', hall_controlers.hall_create_Page);
 
 /* GET create update page */
 router.get('/update', hall_controlers.hall_update_Page);
+
+/* GET delete hall page */
+router.get('/delete', hall_controlers.hall_delete_Page);
 
 module.exports = router;
